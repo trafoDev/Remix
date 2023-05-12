dfc659a30e0fb20547161a1f80f3eba3770fb738
trafoDev <trafo@op.pl>
2023-05-12 12:00:03 +0200

    new

--- contracts/MyBond.sol
+++ contracts/MyBond.sol
@@ -6,18 +6,22 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./MembershipRights.sol";
+import "hardhat/console.sol";
 
 contract MyBond is ERC20, ERC20Burnable, Pausable, Ownable {
     MembershipRights rights;
     ERC20 money;
     uint256 maxSupplay;
     uint256 multiplier;
+    uint256 couponMultiplier;
+    address[] buyers;
 
     constructor(address _rights, address _money, uint256 _maxSupplay) ERC20("Bond", "eBond") {
         rights = MembershipRights(_rights);
         money = ERC20(_money);
         maxSupplay = _maxSupplay;
         multiplier = 100;
+        couponMultiplier = 10;
     }
 
     function decimals() public view virtual override returns (uint8) {
@@ -38,21 +42,37 @@ contract MyBond is ERC20, ERC20Burnable, Pausable, Ownable {
         require(amount > 0 , "Ammount> 0");
         require(money.balanceOf(msg.sender) >= amount * multiplier);
         require(money.allowance(msg.sender, address(this)) >= amount * multiplier, "Allowance too low");
+        if(balanceOf(msg.sender) == 0) {
+            console.log("Adding: %s", msg.sender);
+            console.log("Adding: %u", balanceOf(msg.sender));
+            buyers.push(msg.sender);
+        }
         _mint(msg.sender, amount);
-        money.transferFrom(msg.sender, address(this), amount * multiplier);
-
+        money.transferFrom(msg.sender, owner(), amount * multiplier);
     }
+
 
     function redem(uint256 amount) public {
         require(totalSupply() <= amount, "Not enough bonds issued");
         require(rights.hasRights(msg.sender,  HOLDER), "Not a holder");
         require(amount > 0 , "Ammount> 0");
-        require(money.balanceOf(address(this)) >= amount * multiplier);
+        require(money.balanceOf(owner()) >= amount * multiplier);
         //require(money.allowance(msg.sender, address(this)) >= amount * multiplier, "Allowance too low");
-        _burn(msg.sender, amount);
-        money.transferFrom(address(this), msg.sender, amount * multiplier);
+        _burn(owner(), amount);
+        money.transferFrom(owner(), owner(), amount * multiplier);
+    }
+
+    function coupon() public {
+        require(rights.hasRights(msg.sender,  HOLDER), "Not a holder");
+        require(money.balanceOf(owner()) >= totalSupply() * couponMultiplier);
+
+        for(uint i = 0; i<buyers.length; i++ ) {
+            console.log("Cupon for: %s", buyers[i]);
+            money.transferFrom(owner(), buyers[i], balanceOf(buyers[i]) * couponMultiplier);
+        }
 
     }
+
     function _beforeTokenTransfer(address from, address to, uint256 amount)
         internal
         whenNotPaused
