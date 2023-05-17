f8a85d42b4065cba33e4ee153e3ebe7746f0f15a
trafoDev <trafo@op.pl>
2023-05-17 16:20:18 +0200

    new4

--- contracts/MyBond.sol
+++ contracts/MyBond.sol
@@ -15,12 +15,14 @@ int constant OFFSET19700101 = 2440588;
 contract MyBond is Pausable, Ownable {
     MembershipRights private _rights;
     ERC20 private _money;
+    uint private _moneyDecimal;
     string private _name;
     string private _symbol;
 
     uint256 private _totalSupply1;
     uint256 private _maxSupplay;
     uint256 private _bondPrice;
+    uint256 private _dailyIntrestRate;
     uint256 private _intrestRate;
     address[] private _buyers;
 
@@ -45,11 +47,14 @@ contract MyBond is Pausable, Ownable {
     }
 */
     constructor(address rights, address money) {
-        _rights      = MembershipRights(rights);
-        _money       = ERC20(money);
-        _maxSupplay  = 1000;
-        _bondPrice   = 100;
-        _intrestRate = 10; 
+        _rights       = MembershipRights(rights);
+        _money        = ERC20(money);
+        _maxSupplay   = 1000000;
+        _bondPrice    = 10000;
+        _intrestRate  = 100; 
+        _dailyIntrestRate = 1;
+
+        _moneyDecimal = 2;
         _name = "First Polish Bond based on blockchain"; 
         _symbol = "eBOND"; 
     }
@@ -72,6 +77,32 @@ contract MyBond is Pausable, Ownable {
         month = uint(_month);
         day = uint(_day);
     }
+
+    function _daysFromDate(uint256 year, uint256 month, uint256 day) internal pure returns (uint256 _days) {
+        require(year >= 1970);
+        int256 _year = int256(year);
+        int256 _month = int256(month);
+        int256 _day = int256(day);
+
+        int256 __days = _day - 32075 + (1461 * (_year + 4800 + (_month - 14) / 12)) / 4
+            + (367 * (_month - 2 - ((_month - 14) / 12) * 12)) / 12
+            - (3 * ((_year + 4900 + (_month - 14) / 12) / 100)) / 4 - OFFSET19700101;
+
+        _days = uint256(__days);
+    }
+
+    function _daysFromDateConcatenated(uint256 date) internal pure returns  (uint256 _days) {
+        require(date >= 19710000);
+        int256 _day = int256(date % 100);
+        int256 _month = int256((date / 100) % 100);
+        int256 _year = int256(date / 10000);
+        int256 __days = _day - 32075 + (1461 * (_year + 4800 + (_month - 14) / 12)) / 4
+            + (367 * (_month - 2 - ((_month - 14) / 12) * 12)) / 12
+            - (3 * ((_year + 4900 + (_month - 14) / 12) / 100)) / 4 - OFFSET19700101;
+
+        _days = uint256(__days);
+    }
+
 
     function timestampToDate(uint timestamp) internal pure returns (uint256 date) {
         uint year;
@@ -130,7 +161,7 @@ contract MyBond is Pausable, Ownable {
         require(totalSupply() + amount <= _maxSupplay, "The total number of bonds exceeds the entire supply value.");
         require(_money.balanceOf(msg.sender) >= amount * _bondPrice, "The buyer doesn't have enough money in their account.");
         require(_money.allowance(msg.sender, address(this)) >= amount * _bondPrice, "The account allowance for the trade is set too low.");
-        uint256 _currDate = timestampToDate(block.timestamp);
+        uint256 _currDate = timestampToDate(block.timestamp) -1;
         uint256 _elements = _buys[msg.sender].length; 
         if(_elements == 0) {
             _buyers.push(msg.sender);
@@ -148,16 +179,19 @@ contract MyBond is Pausable, Ownable {
 
     function redem(uint buyDate, uint256 amount) public nonZero(amount){
         bool _setRights = (address(_rights) != address(0));
+        uint256 _now = timestampToDate(block.timestamp);
+        uint _days = _daysFromDateConcatenated(_now) - _daysFromDateConcatenated(buyDate);
         require(_setRights && _rights.hasRights(msg.sender,  ASSET_HOLDER), "Not a holder");
         require(totalSupply() >= amount, "The amout exceeds the total supplay of the asset");
         require(_money.balanceOf(owner()) >= amount * _bondPrice);
         require(_money.allowance(owner(), address(this)) >= amount * _bondPrice, "Allowance too low");
+        require(_now > buyDate, "The bond may be redeemed at least one day after the date of purchase.");
         for(uint i; i < _buys[msg.sender].length; i++) {
             if(_buys[msg.sender][i].date == buyDate) {
                 if(_buys[msg.sender][i].balance >= amount) {
                     _buys[msg.sender][i].balance -= amount;
                     _totalSupply1 -= amount;
-                    _money.transferFrom(owner(), msg.sender, amount * _bondPrice);
+                    _money.transferFrom(owner(), msg.sender, amount * (_bondPrice + (_dailyIntrestRate * _days)));
                     break;
                 }
             }
